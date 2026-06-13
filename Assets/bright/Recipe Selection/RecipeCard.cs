83fd64e8d768975f15d7e9970ad2f3470fc8118c
bright2522 <brightpungporn37@gmail.com>
2026-06-13 16:52:19 +0700

    code 4

--- Assets/bright/Recipe Selection/RecipeCard.cs
+++ Assets/bright/Recipe Selection/RecipeCard.cs
@@ -16,19 +16,23 @@ public class RecipeCard : MonoBehaviour
 
     public void Setup(RecipeSelectionManager.RecipeData data)
     {
-        foodImage.sprite = data.foodImage;
-        nameText.text = data.name;
-        kcalText.text = data.kcal + " kcal";
-        diffText.text = data.difficulty;
+        if (foodImage != null) foodImage.sprite = data.foodImage;
+        if (nameText != null)  nameText.text = data.name;
+        if (kcalText != null)  kcalText.text = data.kcal + " kcal";
+        if (diffText != null)  diffText.text = data.difficulty;
 
         for (int i = 0; i < stars.Length; i++)
-            stars[i].color = i < data.healthStar
-                ? new Color(0.91f, 0.58f, 0.43f)
-                : new Color(0.85f, 0.85f, 0.85f);
+        {
+            if (stars[i] != null)
+                stars[i].color = i < data.healthStar
+                    ? new Color(0.91f, 0.58f, 0.43f)
+                    : new Color(0.85f, 0.85f, 0.85f);
+        }
     }
 
     public void SetSelected(bool isSelected)
     {
-        cardBorder.color = isSelected ? selectedColor : normalColor;
+        if (cardBorder != null)
+            cardBorder.color = isSelected ? selectedColor : normalColor;
     }
 }
